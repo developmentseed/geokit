21e5cfad44e4479b4d9d1e0f07fd74c5e01d05bd
Rub21 <ruben@developmentseed.org>
2018-02-27 10:56:00 -0500

    geokit

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 #!/usr/bin/env node
 
-const fs = require('fs')
 const argv = require('minimist')(process.argv.slice(2));
-const turf = require('@turf/turf');
-const geojson = JSON.parse(fs.readFileSync(argv._[0]).toString());
-var area = 0;
-geojson.features.forEach(function(feature) {
-	area += turf.area(feature) / 1000000;
-})
-console.log(area + ' MK2');
+const area = require('./src/area');
+const bbox2fc = require('./src/bbox2fc');
+const buffer = require('./src/buffer');
+const clip = require('./src/clip');
+var action = argv._[0];
+var file = argv._[1];
+switch (action) {
+	case 'area':
+		area(file);
+		break;
+	case 'bbox2fc':
+		bbox2fc(argv.bbox);
+		break;
+	case 'buffer':
+		buffer(file, argv.unit, argv.radius);
+		break;
+	case 'clip':
+		clip(file, argv._[2]);
+		break;
+	default:
+		console.log('unknown command');
+}
---
+++ src/area.js
@@ -0,0 +1,10 @@
+const fs = require('fs')
+const turf = require('@turf/turf');
+module.exports = function function_name(file) {
+	var area = 0;
+	var geojson = JSON.parse(fs.readFileSync(file).toString());
+	geojson.features.forEach(function(feature) {
+		area += turf.area(feature) / 1000000;
+	})
+	console.log(area + ' MK2');
+}
