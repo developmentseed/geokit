fa3473b990fed495ed4bac9f616b5c9b39c91407
Rub21 <rub2106@gmail.com>
2018-06-28 09:47:50 -0500

    fix parse float

--- src/bbox2fc.js
+++ src/bbox2fc.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const turf = require('@turf/turf');
 module.exports = function(bbox) {
   bbox = bbox.split(',').map(function(item) {
-    return parseInt(item, 10);
+    return parseFloat(item);
   });
   var poly = turf.bboxPolygon(bbox);
   console.log(JSON.stringify(turf.featureCollection([poly])));
