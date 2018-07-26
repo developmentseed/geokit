13a9cf915232618ddf21ae734bd0428d52e62cd0
karitotp <karito@developmentseed.org>
2018-07-26 17:28:55 -0500

    adding test

--- src/countfeature.js
+++ src/countfeature.js
@@ -5,6 +5,7 @@ module.exports = function(file, prop) {
   const propKey = prop.split('=')[0];
   const propValues = prop.split('=')[1].split(',');
   const geojson = JSON.parse(fs.readFileSync(file).toString());
+
   geojson.features.forEach(function(feature) {
     if (feature.properties) {
       let attr = feature.properties[propKey];
@@ -13,5 +14,5 @@ module.exports = function(file, prop) {
       }
     }
   });
-  console.log(propKey + ":" + totfeature);
+  console.log(totfeature);
 };
