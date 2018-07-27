0e80b9becbc4c0f89a240b9f8cbba36cf55666e4
piligab <pilar@developmentseed.org>
2018-07-27 15:07:54 -0500

    Add filterbyprop and line2polygon test

---
+++ test/filterbyprop.test.js
@@ -0,0 +1,31 @@
+const test = require('tape');
+const logInterceptor = require('log-interceptor');
+const path = require('path');
+const cmdFilterbyprop = require('../src/filterbyprop.js');
+const file = path.join(__dirname, '/fixtures/filterbyprop.geojson');
+test('Filterbyprop', function(t) {
+			//t.plan(i);
+			logInterceptor();
+			cmdFilterbyprop(file, 'building=*');
+			let logs = logInterceptor.end();
+
+			const fc = JSON.parse(logs[0]);
+			var a = fc.features.length + fc.features.length;
+			t.plan(a);
+			//t.plan(fc.features.length);
+
+			for (var i = 0; i < fc.features.length; i++) {
+				const propKey = fc.features[i].properties['building'];
+				t.equal(propKey, 'yes', 'ok Propkey');
+				
+				const propType = fc.features[i].geometry.type;
+				
+				if (propType == 'Polygon') {
+					t.equal(propType, 'Polygon', 'ok PropType');
+				}else{
+						t.equal(propType, 'MultiPolygon', 'ok PropType');
+					 }
+			}
+			t.end();
+	});
+
