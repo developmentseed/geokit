41be2f6f97dfadde2220ffc87c78b073f988256e
Rub21 <rub2106@gmail.com>
2018-08-24 17:04:33 -0500

    Fix variables to camelcase

--- src/countbysize.js
+++ src/countbysize.js
@@ -1,14 +1,14 @@
 const fs = require('fs');
 const turf = require('@turf/turf');
-module.exports = function(file, psize) {
-  let totfeature = 0;
+module.exports = function(file, polygonSize) {
+  let totalFeature = 0;
   const geojson = JSON.parse(fs.readFileSync(file).toString());
   geojson.features.forEach(function(feature) {
     let area = turf.area(feature);
     let areaha = (area / 10000).toFixed(0);
-    if (areaha > psize) {
-      totfeature = totfeature + 1;
+    if (areaha >= polygonSize) {
+      totalFeature = totalFeature + 1;
     }
   });
-  console.log(totfeature);
+  console.log(totalFeature);
 };
