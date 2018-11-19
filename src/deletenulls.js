ba9e25acc5c14edd92d95a1259d641b01f53312f
piligab <pilar@developmentseed.org>
2018-11-19 16:26:37 -0500

    Add delete_null

---
+++ src/deletenulls.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+const turf = require('@turf/turf');
+const _ = require('underscore');
+module.exports = function(file) {
+  const geojson = JSON.parse(fs.readFileSync(file).toString());
+
+  for (var i = 0; i < geojson.features.length; i++) {
+    _.each(geojson.features[i].properties, function(val, key) {
+      if(!val && val !== 0){
+        delete geojson.features[i].properties[key]
+      }
+    })
+  }
+  console.log(JSON.stringify(geojson));
+};
