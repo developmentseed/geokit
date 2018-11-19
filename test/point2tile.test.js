7d6ba6f85d95c851f50ecc0cee9fea10499fa739
Rub21 <rub2106@gmail.com>
2018-11-19 15:34:48 -0500

    Add test for point2tile

---
+++ test/point2tile.test.js
@@ -0,0 +1,16 @@
+const test = require('tape');
+const logInterceptor = require('log-interceptor');
+const path = require('path');
+const cmdPoint2tile = require('../src/point2tile');
+const file = path.join(__dirname, '/fixtures/point.geojson');
+test('Point2tile', function(t) {
+  t.plan(1);
+  logInterceptor();
+  cmdPoint2tile(file, 17);
+  setTimeout(function() {
+    let logs = logInterceptor.end();
+    const fc = JSON.parse(logs[0]);
+    t.equal(fc.features[0].geometry.type, 'Polygon', 'ok Polygon type');
+    t.end();
+  }, 300);
+});
