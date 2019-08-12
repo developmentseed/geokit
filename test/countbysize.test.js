098e4cdfb223e4d4b0bb45063152e06a85a9aaa5
piligab <piligab.psq@gmail.com>
2019-08-12 10:45:00 -0500

    update files

--- test/countbysize.test.js
+++ test/countbysize.test.js
@@ -6,9 +6,9 @@ const file = path.join(__dirname, '/fixtures/f_size.geojson');
 test('Countbysize', function(t) {
   t.plan(1);
   logInterceptor();
-  cmdCount(file, 8800);
+  cmdCount(file, 10);
   let logs = logInterceptor.end();
   const count = parseFloat(logs[0]);
-  t.equal(count, 2, 'Ok total farmland larger than 8800 is 2');
+  t.equal(count, 3, 'Ok total farmland larger than 10 is 3');
   t.end();
 });
