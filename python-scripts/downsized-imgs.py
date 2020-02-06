c40f0489e29b62408b725ce8209c8686e09ca0a4
Rub21 <rub2106@gmail.com>
2020-02-06 11:46:38 -0500

    Add documentation for downsized imgs

--- python-scripts/downsized-imgs.py
+++ python-scripts/downsized-imgs.py
@@ -5,17 +5,19 @@ import sys
 import lycon
 import glob
 import os
+import time
+
 reload(sys)
 sys.setdefaultencoding('utf8')
 
+inputDir = sys.argv[1]
+outputDir = sys.argv[1].replace("\/", "")  + "-downsized"
+if not os.path.exists(outputDir):
+    os.makedirs(outputDir)
+
 for file in glob.glob(sys.argv[1]+"/*.jpg"):
-    basename = os.path.basename(file) 
-    # Print the basename name   
-    outputDir = os.path.dirname(file) + "-downsized/"
-    if not os.path.exists(outputDir):
-        os.makedirs(outputDir)
+    basename = os.path.basename(file)
     img = lycon.load(file)
     resized = lycon.resize(img, width=512, height=512, interpolation=lycon.Interpolation.CUBIC)
-    outputFile = outputDir + basename
-    lycon.save(outputDir + basename, resized)
-    print("ok: %s"%(outputFile))
+    lycon.save(outputDir + "/" +basename, resized)
+print("Downsized: %s:%d,%s:%d"%(inputDir,len(glob.glob(inputDir+"/*.jpg")),outputDir,len(glob.glob(outputDir+"/*.jpg"))))
