eebcb2c0371af1da7d33855608e1a59d9ba5fd34
Rub21 <rub2106@gmail.com>
2020-04-02 09:37:34 -0500

    Set lowercase for filtering attributes

--- src/keepattributes.js
+++ src/keepattributes.js
@@ -2,8 +2,8 @@ const fs = require('fs');
 const JSONStream = require('JSONStream');
 const eventStream = require('event-stream');
 const turf = require('@turf/turf');
-module.exports = function (file, keys) {
-  const keepKeys = keys.split(',');
+module.exports = function(file, keys) {
+  const keepKeys = keys.toLowerCase().split(',');
   const fileStream = fs.createReadStream(file, {
     encoding: 'utf8'
   });
@@ -11,10 +11,11 @@ module.exports = function (file, keys) {
   fileStream
     .pipe(JSONStream.parse('features.*'))
     .pipe(
-      eventStream.mapSync(function (data) {
+      eventStream.mapSync(function(data) {
         let newProps = {};
         Object.keys(data.properties).forEach(key => {
-          if (keepKeys.indexOf(key) > -1) {
+          const key_ = key.toLowerCase();
+          if (keepKeys.indexOf(key_) > -1) {
             newProps[key] = data.properties[key];
           }
         });
@@ -23,11 +24,11 @@ module.exports = function (file, keys) {
         return data;
       })
     )
-    .on('error', function (error) {
+    .on('error', function(error) {
       this.emit('end');
       console.log(error);
     })
-    .on('close', function () {
+    .on('close', function() {
       process.stdout.write(JSON.stringify(turf.featureCollection(features)));
       this.emit('end');
     });
