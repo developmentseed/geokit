3e40a1d9b4d63ca46e78eb7f3a3fed08f37a508f
yunica <fmjg21@gmail.com>
2021-08-11 14:55:39 -0500

    add progress bar

--- python-scripts/geokit_py/geo/osm2new.py
+++ python-scripts/geokit_py/geo/osm2new.py
@@ -1,6 +1,9 @@
 """geo.osm2new: Skeleton of a function."""
 
 from lxml import etree
+from tqdm import tqdm
+
+attribs = ["user", "version", "timestamp", "changeset", "uid"]
 
 
 def osm2new(input_osm, output_osm):
@@ -11,61 +14,42 @@ def osm2new(input_osm, output_osm):
     relations = tree.findall(".//relation")
     dict = {}
     index = -5000
-    for node in nodes:
+    for node in tqdm(nodes, desc=f"Process nodes"):
         index = index - 1
         dict[node.attrib["id"]] = index
         node.attrib["id"] = str(index)
         node.attrib["action"] = "modify"
-        if "user" in node.attrib:
-            del node.attrib["user"]
-        if "version" in node.attrib:
-            del node.attrib["version"]
-        if "timestamp" in node.attrib:
-            del node.attrib["timestamp"]
-        if "changeset" in node.attrib:
-            del node.attrib["changeset"]
-        if "uid" in node.attrib:
-            del node.attrib["uid"]
+        for at in attribs:
+            if at in node.attrib:
+                del node.attrib[at]
 
-    for way in ways:
+    for way in tqdm(ways, desc=f"Process ways"):
         index = index - 1
         dict[way.attrib["id"]] = index
         way.attrib["id"] = str(index)
         way.attrib["action"] = "modify"
-        if "user" in way.attrib:
-            del way.attrib["user"]
-        if "version" in way.attrib:
-            del way.attrib["version"]
-        if "timestamp" in way.attrib:
-            del way.attrib["timestamp"]
-        if "changeset" in way.attrib:
-            del way.attrib["changeset"]
-        if "uid" in way.attrib:
-            del way.attrib["uid"]
+        for at in attribs:
+            if at in way.attrib:
+                del way.attrib[at]
         nds = way.findall(".//nd")
         for nd in nds:
             if nd.attrib["ref"] in dict:
                 nd.attrib["ref"] = str(dict[nd.attrib["ref"]])
 
-    for relation in relations:
+    for relation in tqdm(relations, desc=f"Process relations"):
         index = index - 1
         dict[relation.attrib["id"]] = index
         relation.attrib["id"] = str(index)
         relation.attrib["action"] = "modify"
-        if "user" in relation.attrib:
-            del relation.attrib["user"]
-        if "version" in relation.attrib:
-            del relation.attrib["version"]
-        if "timestamp" in relation.attrib:
-            del relation.attrib["timestamp"]
-        if "changeset" in relation.attrib:
-            del relation.attrib["changeset"]
-        if "uid" in relation.attrib:
-            del relation.attrib["uid"]
+        for at in attribs:
+            if at in relation.attrib:
+                del relation.attrib[at]
+
         members = relation.findall(".//member")
         for member in members:
             if member.attrib["ref"] in dict:
                 member.attrib["ref"] = str(dict[member.attrib["ref"]])
+
     xml = b"<?xml version='1.0' encoding='UTF-8'?>\n" + etree.tostring(tree, pretty_print=True, encoding="utf8")
     new_file = open(output_osm, "w")
     new_file.write(xml.decode("utf8"))
