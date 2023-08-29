a1e2d5eb44ed682cbecb575c28a862ef430b2f54
karitotp <karito@developmentseed.org>
2023-08-29 16:55:31 -0500

    minor update

--- python-scripts/geokit_py/mapillary/download_mapillary_imgs.py
+++ python-scripts/geokit_py/mapillary/download_mapillary_imgs.py
@@ -1,9 +1,11 @@
 """mapillary.download_mapillary_imgs: Skeleton of a function."""
 
-import requests
 import os
+
+import requests
 from joblib import Parallel, delayed
 from tqdm import tqdm
+
 from geokit_py.utils.map_utils import read_geojson, write_geojson
 
 access_token = os.environ.get("MAPILLARY_ACCESS_TOKEN")
@@ -13,14 +15,13 @@ def download_img(feature, output_images_path):
     """Download images from Mapillary
 
     Args:
-        feature (dict): feture
+        feature (dict): feature
         output_images_path (str): path to save the images
     Returns:
-        list: features
+        feature
     """
     sequence_id = feature["properties"]["sequence_id"]
     image_folder_path = f"{output_images_path}/{sequence_id}"
-    new_feature = None
     if not os.path.exists(image_folder_path):
         os.makedirs(image_folder_path)
 
@@ -29,7 +30,6 @@ def download_img(feature, output_images_path):
 
     # Check if mapillary image exist and download
     img_file_equirectangular = f"{image_folder_path}/{image_id}.jpg"
-    # img_file_cubemap = f"{image_folder_path}/{image_id}_cubemap.jpg"
 
     header = {"Authorization": "OAuth {}".format(access_token)}
     url = "https://graph.mapillary.com/{}?fields=thumb_1024_url".format(image_id)
@@ -38,6 +38,7 @@ def download_img(feature, output_images_path):
         r = requests.get(url, headers=header)
         data = r.json()
         image_url = data["thumb_1024_url"]
+        feature["properties"]["image_url"] = image_url
 
         with open(img_file_equirectangular, "wb") as handler:
             image_data = requests.get(image_url, stream=True).content
@@ -45,7 +46,7 @@ def download_img(feature, output_images_path):
 
     except Exception as ex:
         print(ex)
-    # return new_feature
+
     return feature
 
 
