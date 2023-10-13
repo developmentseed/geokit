d856560595c6cc163865e80ef9d6238e363d3f28
Junior flores <fmjg21@gmail.com>
2023-10-13 12:03:26 -0300

    update geokit java

--- Dockerfile.java
+++ Dockerfile.java
@@ -1,24 +1,11 @@
-FROM openjdk:11.0.10-buster
-
-RUN apt-get update
+FROM openjdk:17-jdk-buster
 
 # Install pyton
-RUN apt-get install -y \
+RUN apt-get update && apt-get install -y \
     wget \
-    git
+    git \
+    osmosis
 
-# Installing osmosis
-RUN git clone https://github.com/openstreetmap/osmosis.git
-WORKDIR osmosis
-# RUN git checkout 9cfb8a06d9bcc948f34a6c8df31d878903d529fc
-RUN mkdir dist
-RUN ./gradlew assemble
-RUN tar -xvzf "$PWD"/package/build/distribution/*.tgz -C "$PWD"/dist/
-RUN ln -s "$PWD"/dist/bin/osmosis /usr/bin/osmosis
-RUN osmosis --version 2>&1 | grep "Osmosis Version"
-
-# install aws
-RUN apt-get install -y awscli
 
 VOLUME /mnt/data
 WORKDIR /mnt/data
